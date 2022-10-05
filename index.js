60601d981e0e3c6b9390bd740f02a94b7709a160
Hangeony <geonhui95@naver.com>
2022-10-05 17:14:15 +0900

    index html 에서 sidebar 분리

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-import './src/css/style.css';
+//import './src/css/style.css';
 import { navbar } from './src/js/navbar.js';
-
+import { sidebar } from './src/js/sidebar.js';
 navbar();
-
-//import { setupCounter } from './src/js/counter.js';
-
-
-//setupCounter(document.querySelector('#counter'))
+sidebar();
