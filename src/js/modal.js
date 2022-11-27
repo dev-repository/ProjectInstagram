7ed37f8496a41c39062b589bdc1068a5db393597
cheolgyun7 <cjfrbs1256@gmail.com>
2022-11-27 21:38:51 +0900

    '11272'

--- src/js/modal.js
+++ src/js/modal.js
@@ -52,6 +52,8 @@ export function modal() {
     // for(let i =0; i<postComment.length; i++){
     //     // console.log(instaComment[i]);
     //     // console.log(instaId[i]);
-    //     console.log(i)
+        console.log(i)
     // }
+
+
 }
